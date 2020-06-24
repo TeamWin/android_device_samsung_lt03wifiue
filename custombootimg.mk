9e4f5e54f4f4eaac0ca4bb88db89ca689b1f1474
Jenkins <bigbiff@teamw.in>
2020-06-24 01:07:22 +0000

    lt03wifiue: hack to force mkbootimage path to build

--- custombootimg.mk
+++ custombootimg.mk
@@ -1,12 +1,12 @@
-MKBOOTIMG_BIN := out/host/linux-x86/bin/mkbootimg
+MKBOOTIMG_BIN := /builds/out/host/linux-x86/bin/mkbootimg
 
 $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) \
 		$(recovery_ramdisk) \
 		$(recovery_kernel)
 	@echo ----- Creating ramdisk ------
-	chmod 644 out/target/product/lt03wifiue/recovery/root/init.rc
-	chmod 644 out/target/product/lt03wifiue/recovery/root/default.prop
-	(cd out/target/product/lt03wifiue/recovery/root/ && find * | sort | cpio -o -H newc) | gzip -9 > $(recovery_ramdisk)
+	#chmod 644 out/target/product/lt03wifiue/recovery/root/init.rc
+	#chmod 644 out/target/product/lt03wifiue/recovery/root/default.prop
+	(cd /builds/out/target/product/lt03wifiue/recovery/root && find * | sort | cpio -o -H newc) | gzip -9 > $(recovery_ramdisk)
 	@echo ----- Making recovery image ------
 	$(MKBOOTIMG_BIN) --kernel $(TARGET_PREBUILT_KERNEL) --ramdisk $(recovery_ramdisk) --base $(BOARD_KERNEL_BASE) --pagesize $(BOARD_KERNEL_PAGESIZE) $(BOARD_MKBOOTIMG_ARGS) --output $@
 	@echo ----- Made recovery image -------- $@
